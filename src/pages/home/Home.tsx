7a33d2d93d74600d8c5866e883c0849fec2f3fe8
jisu Seo <jseo9732@gmail.com>
2023-04-24 22:01:05 +0900

    Feat: 광고 임시 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -30,6 +30,19 @@ export default function Home() {
       <style.LinkBox to={"/question-name"}>
         <SubmitButton text={"이야기 만들러가기"} />
       </style.LinkBox>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1919598055512436"
+        crossOrigin="anonymous"
+      ></script>
+      {/* <!-- contents/mobile-leaderboard --> */}
+      <ins
+        className="adsbygoogle"
+        style={{ display: "inline-block", width: "320px", height: "50px" }}
+        data-ad-client="ca-pub-1919598055512436"
+        data-ad-slot="1678485541"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       {/* <style.SubSpan>
         지금까지 {numberComma(storyCount)}명이 이야기를 만들었어요.
       </style.SubSpan> */}
